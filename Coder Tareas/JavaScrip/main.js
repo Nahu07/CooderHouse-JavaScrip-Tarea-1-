5486e6917607c80c5ca0fa3075953ac5c46ea9bd
Nahu07 <99214488+Nahu07@users.noreply.github.com>
2026-08-18 16:26:44 -0300

    Add files via upload

---
+++ Coder Tareas/JavaScrip/main.js
@@ -0,0 +1,12 @@
+let nombre = prompt("Ingrese su nombre: ");
+
+let apellido = prompt("Ingrese su apellido: ");
+
+const nacimiento = Number (prompt("Ingrese su año de nacimiento: "));
+
+const añoActual = 2026;
+
+const edad = añoActual - nacimiento; 
+
+alert ("Hola " + nombre + " " + apellido + " tienes " + edad + " años");
+
